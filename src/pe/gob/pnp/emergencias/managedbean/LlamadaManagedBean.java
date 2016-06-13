65660c792e6617c638b49cf481f8b8ac8b9737ec
INTEGRAZI EIRL <INTEGRAZI EIRL>
2016-06-13 01:06:39 -0500

    Avance de pantallas registro de llamada

--- src/pe/gob/pnp/emergencias/managedbean/LlamadaManagedBean.java
+++ src/pe/gob/pnp/emergencias/managedbean/LlamadaManagedBean.java
@@ -1,10 +1,51 @@
 package pe.gob.pnp.emergencias.managedbean;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+
+import pe.gob.pnp.emergencias.model.Llamada;
+import pe.gob.pnp.emergencias.service.CivilService;
+import pe.gob.pnp.emergencias.service.LlamadaService;
 
 @ManagedBean
 @SessionScoped
 public class LlamadaManagedBean {
+	
+	@ManagedProperty(value = "#{llamadaService}")
+	private LlamadaService llamadaService;
+	
+	@ManagedProperty(value = "#{civilService}")
+	private CivilService civilService;
+	
+	Llamada llamada = new Llamada();
+
+	public Llamada getLlamada() {
+		return llamada;
+	}
+
+	public void setLlamada(Llamada llamada) {
+		this.llamada = llamada;
+	}
+
+	public LlamadaService getLlamadaService() {
+		return llamadaService;
+	}
+
+	public void setLlamadaService(LlamadaService llamadaService) {
+		this.llamadaService = llamadaService;
+	}
+	
+	public String registrarLlamada(){
+		
+		llamadaService.getLlamadaRepository().save(llamada);
+		llamada = new Llamada();
+		
+		return "paginas/operador/registroEmergencia";
+	}
+	
+	public String falsaAlarma(){
+		return null;
+	}
 
 }
--- src/pe/gob/pnp/emergencias/managedbean/UsuarioManagedBean.java
+++ src/pe/gob/pnp/emergencias/managedbean/UsuarioManagedBean.java
@@ -1,10 +1,36 @@
 package pe.gob.pnp.emergencias.managedbean;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+
+import pe.gob.pnp.emergencias.service.UsuarioService;
 
 @ManagedBean
 @SessionScoped
 public class UsuarioManagedBean {
 
+	@ManagedProperty(value = "#{usuarioService}")
+	private UsuarioService usuarioService;
+
+	public UsuarioService getUsuarioService() {
+		return usuarioService;
+	}
+
+	public void setUsuarioService(UsuarioService usuarioService) {
+		this.usuarioService = usuarioService;
+	}
+
+	public String ingresar() {
+
+		return "paginas/operador/registroLlamada";
+
+	}
+
+	public String cerrarSesion() {
+
+		return "login";
+
+	}
+	
 }
