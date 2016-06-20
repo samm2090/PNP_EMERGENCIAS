ccf1faab7b5980abd3cac38ecc84b8a60d425ba2
antonny93 <antonnyac.93@gmail.com>
2016-06-20 01:30:07 -0500

    Libreria 1.5 SpringData JPA
    Libreria 1.5 SpringData JPA

--- src/pe/gob/pnp/emergencias/repository/CivilRepository.java
+++ src/pe/gob/pnp/emergencias/repository/CivilRepository.java
@@ -1,6 +1,5 @@
 package pe.gob.pnp.emergencias.repository;
 
-import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
