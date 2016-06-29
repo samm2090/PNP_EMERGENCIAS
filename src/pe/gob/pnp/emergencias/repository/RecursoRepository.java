81afc2ab6f84690eb8fd128efc803b6535bc8794
macrideloayza <macrideloayza@gmail.com>
2016-06-29 16:25:07 -0500

    cambios
    aa

--- src/pe/gob/pnp/emergencias/repository/RecursoRepository.java
+++ src/pe/gob/pnp/emergencias/repository/RecursoRepository.java
@@ -1,8 +1,6 @@
 package pe.gob.pnp.emergencias.repository;
 
-import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
