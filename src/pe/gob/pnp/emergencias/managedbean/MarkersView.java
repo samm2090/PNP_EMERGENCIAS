e07858896149df804ef2b94d8ab45fd6164aabc7
macrideloayza <macrideloayza@gmail.com>
2016-07-04 18:29:07 -0500

    mapa reporte

--- src/pe/gob/pnp/emergencias/managedbean/MarkersView.java
+++ src/pe/gob/pnp/emergencias/managedbean/MarkersView.java
@@ -2,10 +2,12 @@ package pe.gob.pnp.emergencias.managedbean;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.context.FacesContext;
 
 import org.primefaces.model.map.DefaultMapModel;
@@ -13,14 +15,23 @@ import org.primefaces.model.map.LatLng;
 import org.primefaces.model.map.MapModel;
 import org.primefaces.model.map.Marker;
 
+import com.google.common.collect.Lists;
+
+import pe.gob.pnp.emergencias.model.Emergencia;
 import pe.gob.pnp.emergencias.model.EquipoEmergencia;
+import pe.gob.pnp.emergencias.service.EmergenciaService;
+import pe.gob.pnp.emergencias.service.EstadoParteService;
 
 @ManagedBean
 public class MarkersView implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private MapModel simpleModel;
+	private MapModel todosModel;
 	private String centerGeoMap;
+
+	@ManagedProperty(value = "#{emergenciaService}")
+	private EmergenciaService emergenciaService;
 
 	FacesContext context = FacesContext.getCurrentInstance();
 	@SuppressWarnings("rawtypes")
@@ -32,6 +43,15 @@ public class MarkersView implements Serializable {
 		simpleModel = new DefaultMapModel();
 		marcarLugarEmergencia();
 		centrarLugarEmergencia();
+		marcarTodos();
+	}
+
+	public MapModel getTodosModel() {
+		return todosModel;
+	}
+
+	public void setTodosModel(MapModel todosModel) {
+		this.todosModel = todosModel;
 	}
 
 	public MapModel getSimpleModel() {
@@ -74,6 +94,25 @@ public class MarkersView implements Serializable {
 		BigDecimal lon = new BigDecimal(equipoEmergencia1.getEmergencia().getLongitud().toString());
 		String longitudCenter = String.valueOf(lon.doubleValue());
 		centerGeoMap = latitudCenter + "," + longitudCenter;
+	}
+
+	public void marcarTodos()
+	{
+		BigDecimal latitud,longitud;
+		ArrayList<Emergencia> lista = Lists.newArrayList(emergenciaService.getEmergenciaRepository().findAll());
+		for(int i=0;i<lista.size();i++)
+		{
+			latitud = new BigDecimal(lista.get(i).getLatitud().toString());
+			double lat = latitud.doubleValue();
+			longitud = new BigDecimal(lista.get(i).getLongitud().toString());
+			double longi = longitud.doubleValue();
+			
+			LatLng coord3 = new LatLng(lat, longi);
+
+			todosModel.addOverlay(new Marker(coord3, lista.get(i).getEmeObservacion()));		
+		
+		}
+		
 	}
 
 }
