38470b185af8d94667d69a36d962342cb1779b91
macrideloayza <macrideloayza@gmail.com>
2016-07-03 19:46:40 -0500

    reportes

--- src/pe/gob/pnp/emergencias/repository/ParteRepository.java
+++ src/pe/gob/pnp/emergencias/repository/ParteRepository.java
@@ -1,11 +1,16 @@
 package pe.gob.pnp.emergencias.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import pe.gob.pnp.emergencias.model.Parte;
 
 @Repository
 public interface ParteRepository extends CrudRepository<Parte, Long>{
-
+	
+	@Query("select p from Parte p where p.estadoParte.epaId = ?1")
+	public List<Parte> partesExitosas(Long codigoEstado);
+		
 }
